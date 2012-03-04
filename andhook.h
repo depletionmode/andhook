5f5c1615574b22b19c38dd4247629caf062e4fd4
David Kaplan <david@2of1.org>
2012-03-04 12:07:15 +0200

    Doc update

--- andhook.h
+++ andhook.h
@@ -1,7 +1,5 @@
 /* andhook - Android Hooking Framework */
 /* David Kaplan, 2010 (david@2of1.org) */
 
-/* Tested on Android 2.3.6, kernel 2.6.32 */
-
 /* use this fcn within your library's __init() to set up hook */
 void and_hook(void *orig_fcn, void* new_fcn, void **orig_fcn_ptr);
