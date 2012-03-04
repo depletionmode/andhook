a8f1c3508233e6372a5593ec656eca600965c742
David Kaplan <david@2of1.org>
2012-03-04 12:09:36 +0200

    It's 2012git push LOLgit push

--- andhook.h
+++ andhook.h
@@ -1,5 +1,5 @@
 /* andhook - Android Hooking Framework */
-/* David Kaplan, 2010 (david@2of1.org) */
+/* David Kaplan, 2012 (david@2of1.org) */
 
 /* use this fcn within your library's __init() to set up hook */
 void and_hook(void *orig_fcn, void* new_fcn, void **orig_fcn_ptr);
